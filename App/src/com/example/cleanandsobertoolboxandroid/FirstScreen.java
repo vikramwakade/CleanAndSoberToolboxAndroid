e9a65dbb6e4e3d633d393565569767fe8134c757
Jeff Schobelock <schobelock.4@osu.edu>
2014-02-12 15:15:32 -0500

    Not really changed
    Was messing with fragments

--- App/src/com/example/cleanandsobertoolboxandroid/FirstScreen.java
+++ App/src/com/example/cleanandsobertoolboxandroid/FirstScreen.java
@@ -1,8 +1,15 @@
 package com.example.cleanandsobertoolboxandroid;
 
+
+
+
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.support.v4.app.Fragment;
+
+import android.support.v4.app.FragmentTransaction;
 
 public class FirstScreen extends Activity {
 
@@ -10,6 +17,7 @@ public class FirstScreen extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_first_screen);
+		
 	}
 
 	@Override
