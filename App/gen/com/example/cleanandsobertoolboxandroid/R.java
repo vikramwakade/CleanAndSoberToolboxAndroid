269918cca846d393533a694e0ddafab81a0c49e7
Vikram Wakade <vikramw.15@gmail.com>
2014-02-12 15:37:12 -0500

    First commit

--- App/gen/com/example/cleanandsobertoolboxandroid/R.java
+++ App/gen/com/example/cleanandsobertoolboxandroid/R.java
@@ -24,14 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int fragment_container=0x7f080000;
+        public static final int message=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_first_screen=0x7f030000;
+        public static final int all_categories=0x7f030000;
+        public static final int message_view=0x7f030001;
     }
     public static final class menu {
-        public static final int first_screen=0x7f070000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
---
+++ App/src/com/example/cleanandsobertoolboxandroid/SubCategoryFragment.java
@@ -0,0 +1,58 @@
+package com.example.cleanandsobertoolboxandroid;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class SubCategoryFragment extends ListFragment {
+	final static String CATEGORY_POSITION = "cPosition";
+	int cCurrentPosition = -1;
+	
+	OnSubCategorySelectedListener mCallback;
+
+	public interface OnSubCategorySelectedListener {
+		public void onSubCategorySelected(int cPosition, int scPosition);
+	}
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // We need to use a different list item layout for devices older than Honeycomb
+        int layout = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
+                android.R.layout.simple_list_item_activated_1 : android.R.layout.simple_list_item_1;
+
+        // Create an array adapter for the list view, using the Ipsum headlines array
+        Bundle args = getArguments();
+        cCurrentPosition = args.getInt(CATEGORY_POSITION);
+        setListAdapter(new ArrayAdapter<String>(getActivity(), layout, Data.SubCategories[cCurrentPosition]));
+    }
+
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+		
+		mCallback = (OnSubCategorySelectedListener) activity;
+	}
+
+	@Override
+	public void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		
+		mCallback.onSubCategorySelected(cCurrentPosition, position);
+	}
+
+}
