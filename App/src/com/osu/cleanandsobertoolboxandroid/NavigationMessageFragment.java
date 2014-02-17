0a5eb85863f7f489d76b1ab4ec8e2f9e9f5fc0a4
U-Nitish0704\Nitish <Nitish@Nitish0704.(none)>
2014-02-17 16:20:34 -0500

    Navigation drawer

---
+++ App/src/com/osu/cleanandsobertoolboxandroid/NavigationMessageFragment.java
@@ -0,0 +1,67 @@
+package com.osu.cleanandsobertoolboxandroid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class NavigationMessageFragment extends Fragment{
+	final static int disclaimer = 1; //position
+	final static int pyschology = 3;
+	final static String DISCLAIMER = "disclaimer";
+	int curPosition = -1;
+
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+        Bundle savedInstanceState) {
+
+        // If activity recreated (such as from screen rotate), restore
+        // the previous article selection set by onSaveInstanceState().
+        // This is primarily necessary when in the two-pane layout.
+        if (savedInstanceState != null) {
+        	curPosition = savedInstanceState.getInt(DISCLAIMER);
+        }
+
+        
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.message_view, container, false);
+    }
+	
+	@Override
+    public void onStart() {
+        super.onStart();
+
+        // During startup, check if there are arguments passed to the fragment.
+        // onStart is a good place to do this because the layout has already been
+        // applied to the fragment at this point so we can safely call the method
+        // below that sets the article text.
+        Bundle args = getArguments();
+        if (args != null) {
+            // Set article based on argument passed in
+        	updateMessageView(args.getInt(DISCLAIMER));
+        } else if (curPosition != -1) {
+            // Set article based on saved instance state defined during onCreateView
+        	updateMessageView(curPosition);
+        }
+	}
+    
+	public void updateMessageView(int position) {
+		TextView article = (TextView) getActivity().findViewById(R.id.message);
+        article.setMovementMethod(new ScrollingMovementMethod());
+        article.setText(Data.NavigationMessage[position]);
+        curPosition = position;
+            
+    }
+	
+	@Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Save the current article selection in case we need to recreate the fragment
+        outState.putInt(DISCLAIMER, curPosition);
+    }
+    
+}
---
+++ App/src/com/osu/cleanandsobertoolboxandroid/PaypalDonation.java
@@ -0,0 +1,104 @@
+package com.osu.cleanandsobertoolboxandroid;
+
+import java.math.BigDecimal;
+
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+public class PaypalDonation extends Activity {
+	
+	// set to PaymentActivity.ENVIRONMENT_PRODUCTION to move real money.
+    // set to PaymentActivity.ENVIRONMENT_SANDBOX to use your test credentials from https://developer.paypal.com
+    // set to PaymentActivity.ENVIRONMENT_NO_NETWORK to kick the tires without communicating to PayPal's servers.
+    private static final String CONFIG_ENVIRONMENT = PaymentActivity.ENVIRONMENT_NO_NETWORK;
+    
+	// note that these credentials will differ between live & sandbox environments.
+    private static final String CONFIG_CLIENT_ID = "credential from developer.paypal.com"; //"credential-from-developer.paypal.com"
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_paypal_donation);
+		
+		Intent intent = new Intent(this, PayPalService.class);
+
+	    // live: don't put any environment extra
+	    // sandbox: use PaymentActivity.ENVIRONMENT_SANDBOX
+	    intent.putExtra(PaymentActivity.EXTRA_PAYPAL_ENVIRONMENT, CONFIG_ENVIRONMENT);
+
+	    intent.putExtra(PaymentActivity.EXTRA_CLIENT_ID, CONFIG_CLIENT_ID);
+
+	    startService(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.paypal_donation, menu);
+		return true;
+	}
+
+	@Override
+	public void onDestroy() {
+	    stopService(new Intent(this, PayPalService.class));
+	    super.onDestroy();
+	}
+	
+	public void onBuyPressed(View pressed) {
+	    PayPalPayment payment = new PayPalPayment(new BigDecimal("8.75"), "USD", "hipster jeans");
+
+	    Intent intent = new Intent(this, PaymentActivity.class);
+
+	    // comment this line out for live or set to PaymentActivity.ENVIRONMENT_SANDBOX for sandbox
+	    intent.putExtra(PaymentActivity.EXTRA_PAYPAL_ENVIRONMENT, CONFIG_ENVIRONMENT);
+
+	    // it's important to repeat the clientId here so that the SDK has it if Android restarts your
+	    // app midway through the payment UI flow.
+	    intent.putExtra(PaymentActivity.EXTRA_CLIENT_ID, CONFIG_CLIENT_ID);
+
+	    // Provide a payerId that uniquely identifies a user within the scope of your system,
+	    // such as an email address or user ID.
+	    intent.putExtra(PaymentActivity.EXTRA_PAYER_ID, "<someuser@somedomain.com>");
+
+	    intent.putExtra(PaymentActivity.EXTRA_RECEIVER_EMAIL, "<YOUR_PAYPAL_EMAIL_ADDRESS>");
+	    intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+
+	    startActivityForResult(intent, 0);
+	}
+	
+	@Override
+	protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+	    if (resultCode == Activity.RESULT_OK) {
+	        PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+	        if (confirm != null) {
+	            try {
+	                Log.i("paymentExample", confirm.toJSONObject().toString(4));
+
+	                // TODO: send 'confirm' to your server for verification.
+	                // see https://developer.paypal.com/webapps/developer/docs/integration/mobile/verify-mobile-payment/
+	                // for more details.
+
+	            } catch (JSONException e) {
+	                Log.e("paymentExample", "an extremely unlikely failure occurred: ", e);
+	            }
+	        }
+	    }
+	    else if (resultCode == Activity.RESULT_CANCELED) {
+	        Log.i("paymentExample", "The user canceled.");
+	    }
+	    else if (resultCode == PaymentActivity.RESULT_PAYMENT_INVALID) {
+	        Log.i("paymentExample", "An invalid payment was submitted. Please see the docs.");
+	    }
+	}
+}
