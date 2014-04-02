3c9755fb98d8d3a39beb854cff649bfd52530520
Vikram Wakade <vikramw.15@gmail.com>
2014-04-02 13:32:33 -0400

    new JSON

--- App/src/com/osu/cleanandsobertoolboxandroid/MainActivity.java
+++ App/src/com/osu/cleanandsobertoolboxandroid/MainActivity.java
@@ -124,7 +124,7 @@ public class MainActivity extends FragmentActivity
 	        MessageDataSource ds = new MessageDataSource(this);
 	        ds.open();
 	        //ds.EmptyDb();
-	        String result = ds.ProcessJSONFile(R.raw.newest_cleaned_data);
+	        String result = ds.ProcessJSONFile(R.raw.newest_cleaned_data2);
 	        ds.PopulateDbFromJSON(result);
 	        ds.close();
         }
