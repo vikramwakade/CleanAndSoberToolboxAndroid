7b7fedd395160b4a62472c91b6582875c7d8e83c
Nitish Jindal <nitishjindal.dce@gmail.com>
2014-04-02 16:50:05 -0400

    updating the static txt

--- App/src/com/osu/cleanandsobertoolboxandroid/MainActivity.java
+++ App/src/com/osu/cleanandsobertoolboxandroid/MainActivity.java
@@ -13,7 +13,6 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
