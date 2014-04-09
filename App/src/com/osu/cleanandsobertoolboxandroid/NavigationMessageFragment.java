c7b6f28be6be672c7544e30999e1c09bdd5380b5
Nitish Jindal <nitishjindal.dce@gmail.com>
2014-04-09 17:06:12 -0400

    loading json object for disclaimer and psychology

--- App/src/com/osu/cleanandsobertoolboxandroid/NavigationMessageFragment.java
+++ App/src/com/osu/cleanandsobertoolboxandroid/NavigationMessageFragment.java
@@ -15,7 +15,13 @@ public class NavigationMessageFragment extends Fragment{
 	final static int disclaimer = 0; //position
 	final static int psychology = 2;
 	final static String DISCLAIMER = "disclaimer";
+	final static String KEY = "NAVIGATION_MESSAGE";
 	int curPosition = -1;
+	
+	final String[] NavigationMessage = {
+		"Unless otherwise indicated, the quotes and sayings in this App have no known specific author and are taken from research, the author's personal experience, training, and 12 step meetings. If anyone believes a quote or saying or any other mark or logo is not in the public domain, please contact the author immediately at the address provided. None of the information herein creates a counselor patient relationship or should substitute for any other forms of therapy. The author is not liable for any misinterpretations or any injuries claimed from the use of this App. This App provides educational information which may or may not benefit the user, and it is offered as one form of advice and not unequivocal instruction. Copyright 2014  by Clean and Sober Toolbox, Inc. All rights reserved. No part of this App shall be reproduced, stored in a retrieval system, or transmitted by any means, electronic, mechanical, photocopying, recording, or otherwise, without written permission from the publisher. No patent, trademark, copyright or other  liability is assumed with respect to the use of the information contained herein. Although every precaution has been taken in the preparation of this App, the publisher and author assume no responsibility for errors or omissions. Neither is any liability assumed for damages resulting from the use of information contained herein. The author and publisher specifically disclaim any responsibility for any liability, loss, or risk, personal or otherwise, which is incurred as a consequence, directly or indirectly, of the use and application of any of the contents of this App. This App has been registered with the United States Copyright Office.",
+		"Every addict has two brains: a \"dirty brain\" and a \"clean brain.\" The \"dirty brain\" wants to drink, use, or someway act out which ultimately causes trouble. The \"clean brain\" wants to stay clean and sober and free from trouble. This app causes us to use our \"clean brain\" particularly when our \"dirty brain\" is threatening. It substitutes the threatening thought with a truthful, rescuing thought. It changes our perception of a situation from something distressful to something manageable and even positive. It does this with unprecedented ease and speed. The result of changing messages is feeling better, beating cravings, and staying sober."
+	};
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -61,7 +67,9 @@ public class NavigationMessageFragment extends Fragment{
 	public void updateMessageView(int position) {
 		TextView article = (TextView) getActivity().findViewById(R.id.message);
         article.setMovementMethod(new ScrollingMovementMethod());
-        article.setText(Html.fromHtml(Data.NavigationMessage[position]));
+        String message = MainActivity.navigationMessages.getString(KEY+position, NavigationMessage[position]); 
+        //article.setText(Html.fromHtml(Data.NavigationMessage[position]));
+        article.setText(Html.fromHtml(message));
         curPosition = position;
             
     }
